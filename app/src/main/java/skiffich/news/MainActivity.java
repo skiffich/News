3f4979db8643ebd7d289c8a1eb3029a6e9eeaccc
Dmytro Petrenko <skiff120@gmail.com>
2018-10-29 20:07:12 +0200

    add ui elements

--- app/src/main/java/skiffich/news/MainActivity.java
+++ app/src/main/java/skiffich/news/MainActivity.java
@@ -2,12 +2,23 @@ package skiffich.news;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText    editText;
+    private Button      button;
+    private TextView    textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText    = (EditText)findViewById(R.id.editText);
+        button      = (Button)  findViewById(R.id.button);
+        textView    = (TextView)findViewById(R.id.textView);
     }
 }
